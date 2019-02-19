9b7ad7736fb1aa4766a8d63816394098f625a733
Gennady9java <anubis.gen@gmail.com>
2019-02-19 13:36:43 +0200

    modified TodoListThyApplication.java for correct start application in terminal,but...

--- src/main/java/com/springboot/thymeleaf/todo_list_thy/TodoListThyApplication.java
+++ src/main/java/com/springboot/thymeleaf/todo_list_thy/TodoListThyApplication.java
@@ -1,10 +1,21 @@
 package com.springboot.thymeleaf.todo_list_thy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoListThyApplication {
+public class TodoListThyApplication extends SpringBootServletInitializer {
+
+	private  static final Logger log = LoggerFactory.getLogger(TodoListThyApplication.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TodoListThyApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListThyApplication.class, args);
